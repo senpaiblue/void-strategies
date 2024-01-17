cbe3c45e1a7919ea6ab3495c256c5cf74ed363ed
senpaiblue <singhsakcham29@email.com>
2024-01-17 15:46:08 +0530

    bring-page

--- src/components/Home/Bring.js
+++ src/components/Home/Bring.js
@@ -16,9 +16,39 @@ const Bring = () => {
         alt="bring"
         className="max-w-auto px-6 md:hiden lg:hidden"
       />
-      <h1 className="lg:px-32 font-semibold lg:text-xl px-5 lg:text-left text-center text-[#5F2F90]">
+      <h1 className="lg:px-32 montserrat font-semibold lg:text-xl px-5 lg:text-left text-center text-[#5F2F90] pb-8">
         From setting up brand your brand to bringing customers
       </h1>
+      <div className="lg:flex lg:flex-row lg:gap-4 lg:justify-end flex-col lg:items-center justify-center p-4 lg:p-0">
+        <div className="bg-[#5F2F90] flex flex-col items-center justify-center p-8 lg:max-w-[30%]">
+          <h1 className="text-white montserrat font-bold text-2xl">DISCOVER</h1>
+          <p className="text-white montserrat font-medium text-lg text-center">
+            Conducting comprehensive market research and analysis to understand
+            your target audience and the competition
+          </p>
+        </div>
+        <div className="bg-[#5F2F90] flex flex-col items-center justify-center p-8 lg:max-w-[25%]">
+          <h1 className="text-white montserrat font-bold text-2xl">PLAN</h1>
+          <p className="text-white montserrat font-medium text-lg text-center">
+            Develop a tailored strategy and roadmap to achieve your business
+            goals.
+          </p>
+        </div>
+      </div>
+      <div className="lg:flex items-center lg:justify-between justify-center p-4 lg:p-0">
+        <div className="bg-[#ff8513] flex flex-col items-center justify-center p-12 lg:max-w-[40%]">
+          <h1 className="text-white montserrat font-bold text-6xl pb-4">
+            EXECUTE
+          </h1>
+          <p className="text-white montserrat font-medium text-lg text-center">
+            Implementing the strategy through effective campaigns and activities
+            to drive brand awareness and customer acquisition..
+          </p>
+        </div>
+        <h1 className="montserrat  text-shadowBring w-full text-4xl font-bold text-center text-[#5F2F90] md:text-3xl lg:text-5xl lg:w-1/2 md:text-left lg:justify-between 2xl:text-7xl pt-4">
+          BRING 'EM IN
+        </h1>
+      </div>
     </div>
   );
 };
