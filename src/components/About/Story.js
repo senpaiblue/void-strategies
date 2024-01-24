3134e16d8533aaf947b89c362563e6322f6365b0
senpaiblue <singhsakcham29@email.com>
2024-01-24 22:44:12 +0530

    story

--- src/components/About/Story.js
+++ src/components/About/Story.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PhoneBg from "../../../public/assets/About/Mobile/A1.png";
 import Bg from "../../../public/assets/About/A1.png";
+import Bg2 from "../../../public/assets/About/A3.png";
 import Image from "next/image";
 
 const Story = () => {
@@ -12,11 +13,6 @@ const Story = () => {
           alt="backgroundOffer"
           className="w-full p-20 hidden md:flex"
         />
-        <Image
-          src={PhoneBg}
-          alt="backgroundOffer"
-          className="w-full p-4 flex md:hidden"
-        />
         <div className=" absolute left-40 top-40 w-[50vw]">
           <h1 className="font-bold text-6xl text-white pb-2">
           Our Story
@@ -24,7 +20,7 @@ const Story = () => {
           <h2 className="font-semibold text-3xl text-white pb-4">
           What makes us, us.
           </h2>
-          <p className="text-white font-medium leading-normal text-xl md:text-lg">
+          <p className="text-white font-medium leading-normal text-xl md:text-">
             {" "}
             The roots of Void Strategies are deeply entwined with the day-to-day
             realities of Small and Medium-Sized Enterprises (SMEs). Our journey
@@ -60,6 +56,38 @@ const Story = () => {
           </p>
         </div>
       </div>
+      {/* SECOND SECTION */}
+      <div className="bg-white relative hidden md:flex">
+        <Image
+          src={Bg2}
+          alt="backgroundOffer"
+          className="w-full px-20 hidden md:flex"
+        />
+        <div className=" absolute right-80 top-40 w-[50vw]">
+          <p className="text-white font-medium leading-normal text-xl md:text-2xl">
+            {" "}
+            The roots of Void Strategies are deeply entwined with the day-to-day
+            realities of Small and Medium-Sized Enterprises (SMEs). Our journey
+            began not within the confines of a traditional corporate setting,
+            but amid the dynamic and challenging environments where SMEs
+            operate. It was here that our founders, though still undergraduate
+            students at the time, gained invaluable insights into the myriad
+            challenges these businesses regularly face.
+          </p>
+        </div>
+      </div>
+      
+        <div className=" bg-[#F58A23] m-4  rounded-3xl  p-5 flex md:hidden">
+          <p className="text-white font-normal leading-normal text-sm">
+            The roots of Void Strategies are deeply entwined with the day-to-day
+            realities of Small and Medium-Sized Enterprises (SMEs). Our journey
+            began not within the confines of a traditional corporate setting,
+            but amid the dynamic and challenging environments where SMEs
+            operate. It was here that our founders, though still undergraduate
+            students at the time, gained invaluable insights into the myriad
+            challenges these businesses regularly face.
+          </p>
+        </div>
     </section>
   );
 };
