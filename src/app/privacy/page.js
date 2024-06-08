b65a2ee6fae44ea7c264f7e810c484c3822fabbb
senpaiblue <singhsakcham29@email.com>
2024-06-08 17:07:05 +0530

    bug-fix

--- src/app/privacy/page.js
+++ src/app/privacy/page.js
@@ -6,7 +6,8 @@ const privacy = () => {
   return (
     <div className="bg-[#5F2F90] min-h-screen">
       {privacyPolicy.map((item) => (
-        <div className="w-full  lg:px-12 px-4 md:px-8 py-4">
+        <div className="w-full  lg:px-12 px-4 md:px-8 py-4"
+        key={item.heading}>
           <TextGenerateEffect
             className="text-[#ff8513] text-3xl pb-4 font-bold"
             words={item.heading}
--- src/app/terms/page.js
+++ src/app/terms/page.js
@@ -6,7 +6,9 @@ const terms = () => {
   return (
     <div className="bg-[#5F2F90] min-h-screen">
       {Terms.map((item) => (
-        <div className="w-full  lg:px-12 px-4 md:px-8 py-4">
+        <div className="w-full  lg:px-12 px-4 md:px-8 py-4"
+        key={item.heading}
+        >
           <TextGenerateEffect
             className="text-[#ff8513] text-3xl pb-4 font-bold"
             words={item.heading}
